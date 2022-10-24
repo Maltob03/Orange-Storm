5bbed215b8837d314bcccc4a67d9569a949ea546
Maltob03 <46380730+Maltob03@users.noreply.github.com>
2022-10-24 15:06:53 +0200

    First commit

---
+++ Final_product/Final_productApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_productApp.swift
+//  Final_product
+//
+//  Created by Matteo Altobello on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_productApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
