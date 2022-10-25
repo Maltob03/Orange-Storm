bf6902097eb89e0ee7eec28226fc521ecf6d9d2d
excalibur1995 <113535714+excalibur1995@users.noreply.github.com>
2022-10-25 19:25:47 +0200

    NoteView
    I changed the font to bold

--- Orange_Storm/View/NoteView.swift
+++ Orange_Storm/View/NoteView.swift
@@ -14,17 +14,24 @@ struct NoteView: View {
     @State private var inputText4 = ""
     var body: some View {
         VStack{
+            Text("Diary")
+                .fontWeight(.bold)
             Text("July , 5th 2022")
+                .fontWeight(.bold)
             
             List{
                 Text("How do you feel?")
+                    .fontWeight(.bold)
                     
                 TextEditor(text: $inputText)
                 Text("What appened?")
+                    .fontWeight(.bold)
                 TextEditor(text: $inputText2)
                 Text("How did  you feel?")
+                    .fontWeight(.bold)
                 TextEditor(text: $inputText3)
                 Text("What do you think agitated you?")
+                    .fontWeight(.bold)
                 TextEditor(text: $inputText4)
             }
             
