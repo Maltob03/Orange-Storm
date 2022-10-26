f77308656ef105bb979e6d992af6b6904e0e6194
excalibur1995 <113535714+excalibur1995@users.noreply.github.com>
2022-10-26 14:35:29 +0200

    NoteView
    I added padding and i changed the title text. I think this screen is finished it just takes the links with the calendar

--- Orange_Storm/View/NoteView.swift
+++ Orange_Storm/View/NoteView.swift
@@ -12,27 +12,34 @@ struct NoteView: View {
     @State private var inputText2 = ""
     @State private var inputText3 = ""
     @State private var inputText4 = ""
+    @State private var title = "July  5th 2022"
     var body: some View {
         VStack{
             Text("Diary")
                 .fontWeight(.bold)
-            Text("July , 5th 2022")
-                .fontWeight(.bold)
             
             List{
+                Text(title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(30)
                 Text("How do you feel?")
                     .fontWeight(.bold)
                     
                 TextEditor(text: $inputText)
+                    .padding(.bottom,100)
                 Text("What appened?")
                     .fontWeight(.bold)
                 TextEditor(text: $inputText2)
+                    .padding(.bottom,100)
                 Text("How did  you feel?")
                     .fontWeight(.bold)
                 TextEditor(text: $inputText3)
+                    .padding(.bottom,100)
                 Text("What do you think agitated you?")
                     .fontWeight(.bold)
                 TextEditor(text: $inputText4)
+                    .padding(.bottom,100)
             }
             
         }
