f5ed7801c298470c132b820f31cfcfbc76163398
fstellino <fstellino22@fed.idserve.net>
2022-10-28 17:00:52 +0200

    Emoji added
    Now the user can select an emoji in order to classify the day

--- Orange_Storm/Models/Event.swift
+++ Orange_Storm/Models/Event.swift
@@ -12,22 +12,22 @@ import Foundation
 
 struct Event: Identifiable {
     enum EventType: String, Identifiable, CaseIterable {
-        case Panic_Attack, Anxiety, social, sport, unspecified
+        case Joy, Fear, Anger, Sadness, Unspecified
         var id: String {
             self.rawValue
         }
 
         var icon: String {
             switch self {
-            case .Panic_Attack:
-                return "🏦"
-            case .Anxiety:
-                return "🏡"
-            case .social:
-                return "🎉"
-            case .sport:
-                return "🏟"
-            case .unspecified:
+            case .Joy:
+                return "🥳"
+            case .Fear:
+                return "😨"
+            case .Anger:
+                return "😡"
+            case .Sadness:
+                return "😭"
+            case .Unspecified:
                 return "📌"
             }
         }
@@ -51,7 +51,7 @@ struct Event: Identifiable {
         return dateComponents
     }
 
-    init(id: String = UUID().uuidString, eventType: EventType = .unspecified, date: Date, note: String) {
+    init(id: String = UUID().uuidString, eventType: EventType = .Unspecified, date: Date, note: String) {
         self.eventType = eventType
         self.date = date
         self.note = note
@@ -61,12 +61,12 @@ struct Event: Identifiable {
     // Data to be used in the preview
     static var sampleEvents: [Event] {
         return [
-            Event(eventType: .Anxiety, date: Date().diff(numDays: 0), note: "Take dog to groomers"),
+            Event(eventType: .Fear, date: Date().diff(numDays: 0), note: "Take dog to groomers"),
             Event(date: Date().diff(numDays: -1), note: "Get gift for Emily"),
-            Event(eventType: .Anxiety, date: Date().diff(numDays: 6), note: "File tax returns."),
-            Event(eventType: .social, date: Date().diff(numDays: 2), note: "Dinner party at Dave and Janet's"),
-            Event(eventType: .Panic_Attack, date: Date().diff(numDays: -1), note: "Complete Audit."),
-            Event(eventType: .sport, date: Date().diff(numDays: -3), note: "Football Game"),
+            Event(eventType: .Fear, date: Date().diff(numDays: 6), note: "File tax returns."),
+            Event(eventType: .Anger, date: Date().diff(numDays: 2), note: "Dinner party at Dave and Janet's"),
+            Event(eventType: .Joy, date: Date().diff(numDays: -1), note: "Complete Audit."),
+            Event(eventType: .Sadness, date: Date().diff(numDays: -3), note: "Football Game"),
             Event(date: Date().diff(numDays: -4), note: "Plan for winter vacation.")
         ]
     }
